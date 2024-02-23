e675de23491802b711d18f57fff8bb09d20851cb
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-02-23 21:12:50 +0700

    prevent default

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menu = document.getElementsByClassName("menu")[0];
-const hamburgerMenu = document.getElementById ("menu");
+const hamburgerMenu = document.getElementById("menu");
 
-hamburgerMenu.addEventListener("click", function(){
+hamburgerMenu.addEventListener("click", function(e) {
     menu.classList.toggle("active")
+    e.preventDefault()
 })
